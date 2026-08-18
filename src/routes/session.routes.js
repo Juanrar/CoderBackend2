15291e8978ddd39e1390d33825e33e9d28f221c8
juanrar <juanchilorenzo@gmail.com>
2026-08-18 19:47:00 -0300

    feat: add logout functionality to session routes

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { register, login, getCurrentUser } from '../controllers/session.controllers.js';
+import { register, login, logout, getCurrentUser, } from '../controllers/session.controllers.js';
 import { userExists, authMiddleware } from '../middlewares/session.middlewares.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', userExists ,login);
+router.post('/logout', authMiddleware, logout);
 router.get('/current', authMiddleware, getCurrentUser);
 
 export default router;
