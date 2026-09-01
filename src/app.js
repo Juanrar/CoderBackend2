76020413d07d632c862ba36e7613ff4c758a497d
juanrar <juanchilorenzo@gmail.com>
2026-09-01 10:01:54 -0300

    fix: remove unused usersRouter import and route from app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { connectDB } from './config/database.js'
 import { env } from './config/env.js'
-import usersRouter from './routes/user.routes.js'
 import sessionsRouter from './routes/session.routes.js'
 import eventsRouter from './routes/event.routes.js'
 import ticketsRouter from './routes/ticket.routes.js'
@@ -18,7 +17,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/users', usersRouter)
 app.use('/api/sessions', sessionsRouter)
 app.use('/api/events', eventsRouter)
 app.use('/api/tickets', ticketsRouter)
