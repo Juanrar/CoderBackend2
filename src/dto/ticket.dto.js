941b25d81f1cf1c65f30ea535753c7a05962a4a0
juanrar <juanchilorenzo@gmail.com>
2026-09-01 10:01:26 -0300

    fix: correct formatting of reservationCode assignment in TicketDTO

--- src/dto/ticket.dto.js
+++ src/dto/ticket.dto.js
@@ -1,10 +1,11 @@
 export class TicketDTO {
     constructor(ticket) {
         this.id = ticket._id
-        this.reservationCode  = ticket.reservationCode 
+        this.reservationCode = ticket.reservationCode
         this.status = ticket.status
         this.quantity = ticket.quantity
         this.event = ticket.event
+        this.user = ticket.user
         this.createdAt = ticket.createdAt
     }
 }
